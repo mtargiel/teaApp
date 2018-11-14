59037bd0dd22846d5992fe6af89f1ac621f1eec3
Michał Targiel <mtargiel@etisoft.com.pl>
2018-11-14 02:15:45 +0100

    Add project files.

---
+++ Corealate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corealate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
